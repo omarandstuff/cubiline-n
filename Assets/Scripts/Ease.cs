6bde88a5c9e0e6236c12938a646bb0cd16ba47d3
David De Anda <omarandstuff@gmail.com>
2015-11-11 11:13:46 -0600

    Ease script, working on menu actions

---
+++ Assets/Scripts/Ease.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+
+public class Ease : MonoBehaviour
+{
+	//////////////////////////////////////////////////////////////
+	//////////////////////// PARAMETERS //////////////////////////
+	//////////////////////////////////////////////////////////////
+	public EASE_FACE easeFace = EASE_FACE.IN;
+	public EASE_TYPE easeType = EASE_TYPE.SMOOTH;
+	public float easeSmoothTime = 0.5f;
+	public float easeTime = 1.0f;
+	public float easeSpeed = 2.0f;
+
+	public bool easePosition;
+	public bool easeRotation;
+	public bool easeScale;
+
+	public Transform targetInPosition;
+	public Vector3 inPosition;
+	public Transform targetOutPosition;
+	public Vector3 outPosition;
+
+	public Transform targetInRotation;
+	public Vector3 inRotation;
+	public Transform targetOutRotation;
+	public Vector3 outRotation;
+
+	public Transform targetInScale;
+	public Vector3 inScale;
+	public Transform targetOutScale;
+	public Vector3 outScale;
+
+	public Transform targetIn;
+	public Transform targetOut;
+
+	//////////////////////////////////////////////////////////////
+	////////////////////// CONTROL VARIABLES /////////////////////
+	//////////////////////////////////////////////////////////////
+
+	public enum EASE_FACE { IN, OUT }
+	public enum EASE_TYPE { SMOOTH, TIME, SPEED }
+
+	//////////////////////////////////////////////////////////////
+	////////////////////// CONTROL VARIABLES /////////////////////
+	//////////////////////////////////////////////////////////////
+
+	private Vector3 currentPosition;
+	private Vector3 positionVelocity = Vector3.zero;
+	private Vector3 currentRotation;
+	private Vector3 rotationVelocity = Vector3.zero;
+	private Vector3 currentScale;
+	private Vector3 scaleVelocity = Vector3.zero;
+
+	private float easeCurretTime;
+
+	private EASE_FACE lastEaseFace;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////// MONO BEHAVIOR /////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void Start ()
+	{
+		if (targetIn != null)
+		{
+			inPosition = targetIn.transform.localPosition;
+			inRotation = targetIn.localRotation.eulerAngles;
+			inScale = targetIn.localScale;
+		}
+		else
+		{
+			if (targetInPosition != null)
+				inPosition = targetInPosition.localPosition;
+
+			if (targetInRotation != null)
+				inRotation = targetInPosition.localRotation.eulerAngles;
+
+			if (targetInScale != null)
+				inScale = targetInScale.localScale;
+		}
+		if (targetOut != null)
+		{
+			outPosition = targetOut.transform.localPosition;
+			outRotation = targetOut.localRotation.eulerAngles;
+			outScale = targetOut.localScale;
+		}
+		else
+		{
+			if (targetOutPosition != null)
+				outPosition = targetOutPosition.localPosition;
+
+			if (targetOutRotation != null)
+				currentRotation = targetOutPosition.localRotation.eulerAngles;
+
+			if (targetOutScale != null)
+				currentScale = targetOutScale.localScale;
+		}
+
+		if(easeFace == EASE_FACE.IN)
+		{
+			currentPosition = outPosition;
+			currentRotation = outRotation;
+			currentScale = outScale;
+			easeCurretTime = easeTime;
+		}
+		else
+		{
+			currentPosition = inPosition;
+			currentRotation = inRotation;
+			currentScale = inScale;
+			easeCurretTime = 0.0f;
+		}
+		lastEaseFace = easeFace;
+	}
+
+	void FixedUpdate ()
+	{
+		if (easeType == EASE_TYPE.SMOOTH)
+		{
+			if (easePosition) currentPosition = Vector3.SmoothDamp(currentPosition, easeFace == EASE_FACE.IN ? inPosition : outPosition, ref positionVelocity, easeSmoothTime);
+			if (easeRotation) currentRotation = Vector3.SmoothDamp(currentRotation, easeFace == EASE_FACE.IN ? inRotation : outRotation, ref rotationVelocity, easeSmoothTime);
+			if (easeScale) currentScale = Vector3.SmoothDamp(currentScale, easeFace == EASE_FACE.IN ? inScale : outScale, ref scaleVelocity, easeSmoothTime);
+		}
+		else if (easeType == EASE_TYPE.TIME)
+		{
+			easeCurretTime += easeFace == EASE_FACE.IN ? -Time.deltaTime : Time.deltaTime;
+			if (easePosition) currentPosition = Vector3.Lerp(inPosition, outPosition, easeCurretTime / easeTime);
+			if (easeRotation) currentRotation = Vector3.Lerp(inRotation, outRotation, easeCurretTime / easeTime);
+			if (easeScale) currentScale = Vector3.Lerp(inScale, outScale, easeCurretTime / easeTime);
+		}
+		else
+		{
+			if (easePosition) currentPosition += Vector3.Normalize(inPosition - outPosition) * easeSpeed;
+			if (easeRotation) currentRotation += Vector3.Normalize(inRotation - outRotation) * easeSpeed;
+			if (easeScale) currentScale += Vector3.Normalize(inScale - outScale) * easeSpeed;
+		}
+
+		if (easePosition) transform.localPosition = currentPosition;
+		if (easeRotation) transform.localRotation = Quaternion.Euler(currentRotation);
+		if (easeScale) transform.localScale = currentScale;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////// EASE /////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
--- Assets/Scripts/Menu/MenuInteract.cs
+++ Assets/Scripts/Menu/MenuInteract.cs
@@ -8,6 +8,7 @@ public class MenuInteract : MonoBehaviour
 	//////////////////////////////////////////////////////////////
 	public Transform cubeMenu;
 	public Camera menuCamera;
+	public GameObject playModel;
 
 	//////////////////////////////////////////////////////////////
 	//////////////////////// PARAMETERS //////////////////////////
@@ -17,56 +18,93 @@ public class MenuInteract : MonoBehaviour
 	////////////////////// CONTROL VARIABLES /////////////////////
 	//////////////////////////////////////////////////////////////
 
+	public enum MenuAction { PLAY, NONE}
+
+	private MenuAction action;
+
 	private Vector3 currentRotation = Vector3.zero;
 	private Vector3 targetRotation = Vector3.zero;
 	private Vector3 inRotation = Vector3.zero;
 	private Vector3 rotationVelocity = Vector3.zero;
 
-	private Vector3 targetScale = new Vector3(11, 11, 11);
-	private Vector3 curretScale = new Vector3(11, 11, 11);
+	private GameObject targetModel;
+	private Vector3 baseScale;
+	private Vector3 targetScale;
+	private Vector3 curretScale;
 	private Vector3 scaleVelocity = Vector3.zero;
+
+	private Vector3 playModelBaseScale;
+
+	private MenuAction pressAction;
 
 	////////////////////////////////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////// MONO BEHAVIOR /////////////////////////////////////////
 	////////////////////////////////////////////////////////////////////////////////////////////////
 
-	private void Start()
+	void Start()
 	{
 		menuCamera.transform.position = new Vector3(-20, 0, -80);
+		targetModel = playModel;
+		
+		playModelBaseScale = playModel.transform.localScale;
+		baseScale = playModelBaseScale;
+		targetScale = baseScale;
+		curretScale = targetScale;
 	}
 
-	public void FixedUpdate()
+	void FixedUpdate()
 	{
 		currentRotation = Vector3.SmoothDamp(currentRotation, targetRotation + inRotation, ref rotationVelocity, 0.15f);
-		curretScale = Vector3.SmoothDamp(curretScale, targetScale, ref scaleVelocity, 0.15f);
+		curretScale = Vector3.SmoothDamp(curretScale, targetScale, ref scaleVelocity, 0.08f);
 
 		cubeMenu.localRotation = Quaternion.Euler(currentRotation);
-		cubeMenu.localScale = curretScale;
+		targetModel.transform.localScale = curretScale;
 	}
 
-	public void OnMouseDown()
+	void OnMouseDown()
 	{
-		targetScale = new Vector3(8.0f, 8.0f, 8.0f);
+		targetScale = baseScale * 0.7f;
 	}
 
-	public void OnMouseDrag()
+	void OnMouseDrag()
 	{
-		inRotation.y -= Input.GetAxis("Horizontal Mouse") * 0.3f;
+		float axis = Input.GetAxis("Horizontal Mouse");
+		inRotation.y -= axis * 0.3f;
+		if (axis != 0)
+			targetScale = baseScale;
 	}
 
-	public void OnMouseUp()
+	void OnMouseUp()
 	{
 		targetRotation.y += (inRotation.y > 0 ? Mathf.Ceil(((int)inRotation.y / 45) / 2.0f) : Mathf.Floor(((int)inRotation.y / 45) / 2.0f)) * 90;
-		targetScale = new Vector3(11.0f, 11.0f, 11.0f);
 		inRotation.y = 0;
+		targetScale = baseScale;
+	}
+
+	void OnMouseUpAsButton()
+	{
+		if(pressAction == action)
+		{
+			if (action == MenuAction.PLAY)
+				Application.LoadLevelAsync(1);
+		}
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////
-	///////////////////////////////////////////// SETUP ////////////////////////////////////////////
+	///////////////////////////////////////////// MENU /////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////////////////////////////////////
 
-	public void Reset(float arenaSize)
+	public void SetAction(Vector3 rotation)
 	{
+		rotation.y = Mathf.Repeat(rotation.y, 360);
 
+		if(rotation.y == 0 || rotation.y == 360)
+		{
+			action = MenuAction.PLAY;
+		}
+		else
+		{
+			action = MenuAction.NONE;
+		}
 	}
 }
