240c056e237d0c778c248e4a2206b06ba07e6c05
David De Anda <omarandstuff@gmail.com>
2015-11-12 00:15:57 -0600

    Smooth menu to game, menu actions system

---
+++ Assets/Scripts/Menu/InteractiveMenu.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+
+public class InteractiveMenu : MonoBehaviour
+{
+	//////////////////////////////////////////////////////////////
+	///////////////////////// COMPONENTS /////////////////////////
+	//////////////////////////////////////////////////////////////
+	public OrbitAndLook menuCamera;
+	public EaseTransform topLight;
+	public EaseTransform cubilineTilte;
+	public EaseTransform actionTitleBase;
+	public TextMesh actionTitle;
+	public EaseTransform playModel;
+
+	//////////////////////////////////////////////////////////////
+	//////////////////////// PARAMETERS //////////////////////////
+	//////////////////////////////////////////////////////////////
+
+	public enum MENU_ACTION { PLAY, NONE }
+
+	public MENU_ACTION selectedAction;
+	public MENU_ACTION goingAction;
+
+	//////////////////////////////////////////////////////////////
+	////////////////////// CONTROL VARIABLES /////////////////////
+	//////////////////////////////////////////////////////////////
+
+	//////////////////////// CUBE ROTATION ///////////////////////
+
+	private Vector2 actionRotation = Vector3.zero;
+	private Vector3 currentRotation = Vector3.zero;
+	private Vector3 targetRotation = Vector3.zero;
+	private Vector3 inRotation = Vector3.zero;
+	private Vector3 rotationVelocity = Vector3.zero;
+
+	/////////////////////////// ACTION ///////////////////////////
+	private EaseTransform currentModelAction;
+	private bool actionReady;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////// MONO BEHAVIOR /////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void Start()
+	{
+		menuCamera.transform.localPosition = new Vector3(0, 0, -80);
+		SetAction();
+		goingAction = MENU_ACTION.NONE;
+	}
+
+	void FixedUpdate()
+	{
+		if(goingAction == MENU_ACTION.NONE)
+		{
+			currentRotation = Vector3.SmoothDamp(currentRotation, targetRotation + inRotation, ref rotationVelocity, 0.15f);
+			transform.localRotation = Quaternion.Euler(currentRotation);
+		}
+		else if(goingAction == MENU_ACTION.PLAY)
+		{
+			if (menuCamera.transform.position.z <= -50)
+				Application.LoadLevel(1);
+		}
+	}
+
+	void OnMouseDown()
+	{
+		actionReady = true;
+		if (currentModelAction != null) currentModelAction.easeFace = EaseTransform.EASE_FACE.OUT;
+	}
+
+	void OnMouseDrag()
+	{
+		float axis = Input.GetAxis("Horizontal Mouse");
+		inRotation.y -= axis * 0.3f;
+		actionRotation.y = targetRotation.y + (inRotation.y > 0 ? Mathf.Ceil(((int)inRotation.y / 45) / 2.0f) : Mathf.Floor(((int)inRotation.y / 45) / 2.0f)) * 90;
+		SetAction();
+		if (axis != 0)
+		{
+			actionReady = false;
+			if (currentModelAction != null) currentModelAction.easeFace = EaseTransform.EASE_FACE.IN;
+		}
+	}
+
+	void OnMouseUp()
+	{
+		targetRotation.y += (inRotation.y > 0 ? Mathf.Ceil(((int)inRotation.y / 45) / 2.0f) : Mathf.Floor(((int)inRotation.y / 45) / 2.0f)) * 90;
+		inRotation.y = 0;
+	}
+
+	void OnMouseUpAsButton()
+	{
+		if (actionReady)
+		{
+			if (selectedAction == MENU_ACTION.PLAY)
+			{
+				cubilineTilte.easeFace = EaseTransform.EASE_FACE.OUT;
+				actionTitleBase.easeFace = EaseTransform.EASE_FACE.OUT;
+				menuCamera.automaticDistance = false;
+				menuCamera.distance = 51.0f;
+				goingAction = MENU_ACTION.PLAY;
+				playModel.easePosition = true;
+				playModel.easeFace = EaseTransform.EASE_FACE.OUT;
+				topLight.easeFace = EaseTransform.EASE_FACE.OUT;
+				GetComponent<EaseTransform>().easeFace = EaseTransform.EASE_FACE.OUT;
+				GetComponent<Collider>().enabled = false;
+			}
+		}
+		else
+		{
+			if (currentModelAction != null) currentModelAction.easeFace = EaseTransform.EASE_FACE.IN;
+		}
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////// ACTION ////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void SetAction()
+	{
+		float fixedY = Mathf.Repeat(actionRotation.y, 360.0f);
+		if(fixedY == 0.0f || fixedY == 360.0f)
+		{
+			selectedAction = MENU_ACTION.PLAY;
+			currentModelAction = playModel;
+			actionTitle.text = "Play";
+		}
+		else
+		{
+			selectedAction = MENU_ACTION.NONE;
+			currentModelAction = null;
+			actionTitle.text = "None";
+		}
+	}
+
+}
