5d2b40d1bfb632d089720575eb2a5b03d31b5d7b
David De Anda <omarandstuff@gmail.com>
2015-11-17 19:28:30 -0600

    Testing stuff

--- Assets/Scripts/Cubiline Game/Menues/FinishMenuController.cs
+++ Assets/Scripts/Cubiline Game/Menues/FinishMenuController.cs
@@ -1,35 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class FinishMenuController : MonoBehaviour {
-
-	public Image scoreHolder;
-	public Text scoreText;
-	public float smoothTime;
+public class FinishMenuController : MonoBehaviour
+{
 
 
-	public int score
-	{
-		set
-		{
-			targetScore = value + 0.8f;
-		}
-	}
-
-	private float targetScore = 150.8f;
-	private float currentScore;
-	private float velocity;
-
-	void Update ()
-	{
-		if (currentScore <= targetScore - 0.8f)
-		{
-			currentScore = Mathf.SmoothDamp(currentScore, targetScore, ref velocity, smoothTime);
-			scoreText.text = ((int)currentScore).ToString();
-		}
-		else
-		{
-			//scoreHolder.GetComponent<EaseTransform>().inScale = new Vector3(0.2f, 0.2f, 0.2f);
-		}
-	}
 }
