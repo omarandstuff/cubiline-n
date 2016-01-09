6442eb637ef196fb735472addb86984c7ae9a41e
David De Anda <omarandstuff@gmail.com>
2016-01-09 00:46:07 -0600

    Working with Facebook log in, and player view and class

---
+++ Assets/Scripts/Application/CubilinePlayer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using UnityEngine;
+using Facebook.Unity;
+using System.Collections.Generic;
+
+public class CubilinePlayer
+{
+	public static bool playerLogedIn;
+
+
+	private static Action<bool> logInCallback = null;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////// LOG IN /////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static void LogInPlayer(Action<bool> callback = null)
+	{
+		if (playerLogedIn) return;
+		logInCallback = callback;
+#if UNITY_WEBGL && !UNITY_EDITOR
+		if (!FB.IsInitialized)
+		{
+			// Initialize the Facebook SDK
+			FB.Init(OnInitComplete, OnHideUnity);
+		}
+		else
+		{
+			// Already initialized, signal an app activation App Event
+			FB.ActivateApp();
+		}
+#endif
+	}
+
+	private void OnInitComplete()
+	{
+		Debug.Log(string.Format("OnInitCompleteCalled IsLoggedIn='{0}' IsInitialized='{1}'", FB.IsLoggedIn, FB.IsInitialized));
+
+		// LogIn
+		if(FB.IsInitialized)
+			FB.LogInWithReadPermissions(new List<string>() { "public_profile", "email", "user_friends" }, HandleResult);
+	}
+
+	private void OnHideUnity(bool isGameShown)
+	{
+		Debug.Log(string.Format("Success Response: OnHideUnity Called {0}\n", isGameShown));
+	}
+
+	private void HandleResult(ILoginResult result)
+	{
+		if (result == null)
+		{
+			Debug.Log("Null Response");
+			return;
+		}
+
+		// Some platforms return the empty string instead of null.
+		if (!string.IsNullOrEmpty(result.Error))
+		{
+			Debug.Log("Error Response: " + result.Error);
+		}
+		else if (result.Cancelled)
+		{
+			Debug.Log("Cancelled Response: " + result.RawResult);
+		}
+		else if (!string.IsNullOrEmpty(result.RawResult))
+		{
+			Debug.Log("Success Response: " + result.RawResult);
+		}
+		else
+		{
+			Debug.Log("Empty Response");
+		}
+		if (FB.IsLoggedIn)
+		{
+			// AccessToken class will have session details
+			var aToken = AccessToken.CurrentAccessToken;
+			// Print current access token's User ID
+			Debug.Log(aToken.UserId);
+
+			// Print current access token's granted permissions
+			foreach (string perm in aToken.Permissions)
+			{
+				Debug.Log(perm);
+			}
+
+			playerLogedIn = true;
+		}
+		else
+		{
+			Debug.Log("User cancelled login");
+			playerLogedIn = false;
+		}
+		logInCallback(playerLogedIn);
+	}
+}
---
+++ Assets/Scripts/Menues/Main Menu/PlayerActionController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine.UI;
+using Facebook.Unity;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerActionController : ActionContentController
+{
+	//////////////////////////////////////////////////////////////
+	///////////////////////// COMPONENTS /////////////////////////
+	//////////////////////////////////////////////////////////////
+
+	public ScrollRect scrollView;
+	public EaseTextOpasity[] emergentTexts;
+	public EaseImageOpasity[] emergentImages;
+	public GameObject contentView;
+	public GameObject notLoggedInViewPrefab;
+	public GameObject loggedInViewPrefab;
+
+	//////////////////////////////////////////////////////////////
+	///////////////////// CONTROL VARIABLES //////////////////////
+	//////////////////////////////////////////////////////////////
+
+	private GameObject currentLogView;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////// MONO BEHAVIOR /////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////
+
+	void Awake()
+	{
+		CubilinePlayer.LogInPlayer(logInCallback);
+	}
+
+	void Start()
+	{
+		currentLogView = Instantiate(notLoggedInViewPrefab);
+		currentLogView.transform.SetParent(contentView.transform);
+		currentLogView.transform.localRotation = transform.localRotation;
+		currentLogView.GetComponent<RectTransform>().position = Vector3.zero;
+	}
+
+	public override void Select()
+	{
+		GetComponent<GraphicRaycaster>().enabled = true;
+		foreach (EaseImageOpasity ei in emergentImages)
+			ei.easeFace = EaseFloat.EASE_FACE.IN;
+		foreach (EaseTextOpasity et in emergentTexts)
+			et.easeFace = EaseFloat.EASE_FACE.IN;
+	}
+
+	public override void Unselect()
+	{
+		GetComponent<GraphicRaycaster>().enabled = false;
+		foreach (EaseImageOpasity ei in emergentImages)
+			ei.easeFace = EaseFloat.EASE_FACE.OUT;
+		foreach (EaseTextOpasity et in emergentTexts)
+			et.easeFace = EaseFloat.EASE_FACE.OUT;
+		base.Unselect();
+	}
+
+	public override void Enter()
+	{
+
+	}
+
+	public override void Leave()
+	{
+
+	}
+
+	public void OkAction()
+	{
+		Unselect();
+	}
+
+	public void CancelAction()
+	{
+		Unselect();
+	}
+
+	private void logInCallback(bool logedin)
+	{
+
+	}
+}
