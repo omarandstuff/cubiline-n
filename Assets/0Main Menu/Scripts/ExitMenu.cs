4a2b09440e03c978211bca4b2dddc53436473e8d
David De Anda <omarandstuff@gmail.com>
2016-01-24 00:55:16 -0600

    Exit promt

---
+++ Assets/0Main Menu/Scripts/ExitMenu.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitMenu : MonoBehaviour
+{
+	public GameObject exitPromtPrefab;
+
+	private bool menuKey;
+	private GameObject exitPromt;
+
+	void OnGUI()
+	{
+		Event e = Event.current;
+		if (e.type == EventType.KeyDown)
+		{
+			if (e.keyCode == KeyCode.Escape && !menuKey) // Menu
+			{
+				menuKey = true;
+
+				if (exitPromt != null)
+				{
+					exitPromt.GetComponent<ExitPromt>().GoOut();
+					Destroy(exitPromt.gameObject, 0.5f);
+					GetComponent<Collider>().enabled = true;
+					GetComponent<MenuController>().navigationLeft.GetComponent<Collider>().enabled = true;
+					GetComponent<MenuController>().navigationRight.GetComponent<Collider>().enabled = true;
+				}
+				else
+				{
+					exitPromt = Instantiate(exitPromtPrefab);
+					exitPromt.GetComponent<ExitPromt>().exitMenu = this;
+					GetComponent<Collider>().enabled = false;
+					GetComponent<MenuController>().navigationLeft.GetComponent<Collider>().enabled = false;
+					GetComponent<MenuController>().navigationRight.GetComponent<Collider>().enabled = false;
+				}
+			}
+		}
+		else if (e.type == EventType.keyUp)
+		{
+			if (e.keyCode == KeyCode.Escape)
+				menuKey = false;
+		}
+	}
+
+	public void CancelAction()
+	{
+		menuKey = false;
+		exitPromt.GetComponent<ExitPromt>().GoOut();
+		Destroy(exitPromt.gameObject, 0.5f);
+		GetComponent<Collider>().enabled = true;
+		GetComponent<MenuController>().navigationLeft.GetComponent<Collider>().enabled = true;
+		GetComponent<MenuController>().navigationRight.GetComponent<Collider>().enabled = true;
+	}
+
+	public void OkAction()
+	{
+		Application.Quit();
+	}
+}
---
+++ Assets/0Main Menu/Scripts/ExitPromt.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitPromt : MonoBehaviour
+{
+	public ExitMenu exitMenu;
+	public GameObject bigBack;
+	public GameObject back;
+	public GameObject title;
+	public GameObject okButton;
+	public GameObject cancelButton;
+
+	
+	public void GoOut()
+	{
+		bigBack.GetComponent<EaseImageOpasity>().easeFace = EaseFloat.EASE_FACE.OUT;
+		back.GetComponent<EaseImageOpasity>().easeFace = EaseFloat.EASE_FACE.OUT;
+		back.GetComponent<EaseScale>().easeFace = EaseVector3.EASE_FACE.OUT;
+		title.GetComponent<EaseTextOpasity>().easeFace = EaseFloat.EASE_FACE.OUT;
+		okButton.GetComponent<EaseImageOpasity>().easeFace = EaseFloat.EASE_FACE.OUT;
+		okButton.transform.GetChild(0).GetComponent<EaseTextOpasity>().easeFace = EaseFloat.EASE_FACE.OUT;
+		cancelButton.GetComponent<EaseImageOpasity>().easeFace = EaseFloat.EASE_FACE.OUT;
+		cancelButton.transform.GetChild(0).GetComponent<EaseTextOpasity>().easeFace = EaseFloat.EASE_FACE.OUT;
+	}
+
+	public void CancelAction()
+	{
+		exitMenu.CancelAction();
+	}
+
+	public void OkAction()
+	{
+		exitMenu.OkAction();
+	}
+}
